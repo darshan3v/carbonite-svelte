a24911f119ce7cee158c1604f42cc8ffdda77e7d
Darshan V <darshanv3v@gmail.com>
2022-11-18 21:42:25 +0530

    route protection + extra redirection added except for admin page

--- src/routes/Company/+page.ts
+++ src/routes/Company/+page.ts
@@ -1,14 +1,31 @@
 import { browser } from '$app/environment';
-import { getWalletSelector } from '$src/wallet/wallet';
+import type { AccountId } from '$src/wallet/types';
+import {
+	getWalletSelector,
+	is_carbonite_company_acc,
+	is_carbonite_user_acc
+} from '$src/wallet/wallet';
+import type { WalletSelector } from '@near-wallet-selector/core';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
 	if (browser) {
-		const isSignedIn = (await getWalletSelector()).isSignedIn();
+		const walletSelector: WalletSelector = await getWalletSelector();
+		const isSignedIn: boolean = walletSelector.isSignedIn();
+
+		let accountId: AccountId;
 
 		if (isSignedIn) {
-			console.log('check if company type then redirect to dashboard or else accordingly');
+			accountId = walletSelector.store.getState().accounts[0].accountId;
+
+			if (is_carbonite_user_acc(accountId)) {
+				throw redirect(307, '/User/Dashboard');
+			} else if (is_carbonite_company_acc(accountId)) {
+				throw redirect(307, '/Company/Dashboard');
+			}
+			// console.log('check if company type then redirect to dashboard or else accordingly');
+		} else {
 			throw redirect(307, '/');
 		}
 	}
--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -10,6 +10,7 @@ import { TESTNET_NETWORK_ID, TESTNET_CONTRACT_ID } from '$src/wallet/constants';
 // importing types
 import type { WalletConfig } from '$src/wallet/near-wallet';
 import type { WalletSelector } from '@near-wallet-selector/core';
+import type { AccountId } from './types';
 
 export const walletConfig: WalletConfig = {
 	network: TESTNET_NETWORK_ID,
@@ -17,9 +18,9 @@ export const walletConfig: WalletConfig = {
 	createAccessKeyFor: TESTNET_CONTRACT_ID
 };
 
-export let nearWallet: NearWallet
+export let nearWallet: NearWallet;
 
-export async function  getWalletSelector() {
+export async function getWalletSelector() {
 	return await NearWallet.WithWalletSelector(walletConfig);
 }
 
@@ -28,11 +29,25 @@ export async function setupWallet(walletConfig: WalletConfig): Promise<boolean> 
 
 	nearWallet = new NearWallet(walletConfig, walletSelector);
 
-	return await nearWallet.startUp()
+	return await nearWallet.startUp();
+}
+
+export function is_carbonite_user_acc(accountId: AccountId) {
+	const username = accountId.slice(0, accountId.indexOf('.'));
+	const subaccount = accountId.slice(accountId.indexOf('.') + 1);
+
+	return username !== '' && subaccount === TESTNET_CONTRACT_ID && !username.endsWith('-Co');
+}
+
+export function is_carbonite_company_acc(accountId: AccountId) {
+	const company_name = accountId.slice(0, accountId.indexOf('.'));
+	const subaccount = accountId.slice(accountId.indexOf('.') + 1);
+
+	return company_name.endsWith('-Co') && subaccount === TESTNET_CONTRACT_ID;
 }
 
 // Other extra stuff ('')!
-export function setup(){
+export function setup() {
 	window.Buffer = Buffer;
 	console.log('env', env);
 	window.process = {
@@ -40,3 +55,4 @@ export function setup(){
 	};
 	window.process.env = env;
 }
+
