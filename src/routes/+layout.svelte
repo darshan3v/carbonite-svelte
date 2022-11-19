d2d66cb6407c7503f5cd2c08835cca178dd935de
Akhil <akhile76@gmail.com>
2022-11-19 10:28:08 +0530

    add top nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<!-- top nav with all linkes -->
+<nav>
+	<a href="/">Home</a>
+	<a href="/User">User</a>
+	<!-- User Dashboard -->
+	<a href="/User/Dashboard">User Dashboard</a>
+	<!-- company  -->
+	<a href="/Company">Company</a>
+	<!-- company Dashboard -->
+	<a href="/Company/Dashboard">Company Dashboard</a>
+</nav>
+<slot />
+
+<!-- style top nav -->
+<style>
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		background: #ff3e00;
+	}
+	nav a {
+		color: #fff;
+		text-decoration: none;
+	}
+</style>
