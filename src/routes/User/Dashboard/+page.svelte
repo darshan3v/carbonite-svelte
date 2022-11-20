7c163779ffdbc9b1292fc7a167c2d08f2a5b5d51
Akhil <akhile76@gmail.com>
2022-11-21 01:50:12 +0530

    feat(user-dashboard): added task list to users

--- src/routes/User/Dashboard/+page.svelte
+++ src/routes/User/Dashboard/+page.svelte
@@ -1,12 +1,12 @@
 <!-- submit task (inputs:(task_id,(Submission(line 57))) -> outputs(true or false or error)) -->
 <script lang="ts">
-	import type { JsonToken, Task, TokenMetadata } from '$src/wallet/structs_enums';
+	import type { JsonTask, JsonToken, Task, TokenMetadata } from '$src/wallet/structs_enums';
 	import type { Vec } from '$src/wallet/types';
 	import { get_all_tasks_list, nft_metadata, nft_tokens_for_owner } from '$src/wallet/view';
 	import { nearWallet, setupWallet, walletConfig } from '$src/wallet/wallet';
 	import { onMount } from 'svelte';
 
-	let taskList: Task[] = [];
+	let tasklist: Promise<Vec<JsonTask>> = new Promise((resolve) => resolve([]));
 
 	let nft: JsonToken;
 
@@ -41,8 +41,11 @@
 	// function to get all tasks from the contract by calling get_all_tasks_list(from_index,limit) until it returns empty array
 	async function getTasks() {
 		//TODO:do later
-		let tasklist = await nearWallet.get_all_tasks_list({ from_index: '0', limit: 25 });
-		console.log('tasklist', tasklist);
+		tasklist = nearWallet.get_all_tasks_list({ from_index: '0', limit: 25 });
+
+		tasklist.then((tasks) => {
+			console.log('tasks', tasks);
+		});
 	}
 </script>
 
@@ -73,3 +76,64 @@
 	<h3>Tasks Completed : {tasks_completed.length === 0 ? 'No Tasks Completed' : tasks_completed}</h3>
 	<h3>Total Tasks Completed : {total_tasks_completed}</h3>
 {/if}
+
+<h1>TaskList</h1>
+<!-- wait for tasklist -->
+{#await tasklist}
+	<p>loading...</p>
+{:then tasks}
+	<!-- check each task state and seprate them in seprate groups  
+			task.task.task_state = 'Open' | 'Pending' | 'Completed' | 'Expired' | 'Overdue' | 'Payed'; -->
+	{#each ['Open', 'Pending', 'Completed', 'Expired', 'Overdue', 'Payed'] as state}
+		<h1>{state}</h1>
+		{#each tasks as task}
+			{#if task.task.task_state === state}
+				<h3>Task ID</h3>
+				<div>{task.task_id}</div>
+				<div>Company ID</div>
+				<div>{task.task.company_id}</div>
+				<div>Deadline</div>
+				<div>{task.task.deadline}</div>
+				<div>Person Assigned</div>
+				<div>{task.task.person_assigned}</div>
+				<div>Task State</div>
+				<div>{task.task.task_state}</div>
+				<div>FT Contract ID</div>
+				<div>{task.task.ft_contract_id}</div>
+				<div>Reward</div>
+				<div>{task.task.reward}</div>
+
+				<h4>Task Details</h4>
+				<div>{JSON.stringify(task.task.task_details)}</div>
+
+				<button on:click={() => nearWallet.accept_invite({ task_id: task.task_id })}
+					>Accept Invite</button
+				>
+			{/if}
+		{/each}
+	{/each}
+
+	<!-- {#each tasks as task}
+		<h2>Task ID</h2>
+		<div>{task.task_id}</div>
+		<div>Company ID</div>
+		<div>{task.task.company_id}</div>
+		<div>Deadline</div>
+		<div>{task.task.deadline}</div>
+		<div>Person Assigned</div>
+		<div>{task.task.person_assigned}</div>
+		<div>Task State</div>
+		<div>{task.task.task_state}</div>
+		<div>FT Contract ID</div>
+		<div>{task.task.ft_contract_id}</div>
+		<div>Reward</div>
+		<div>{task.task.reward}</div>
+
+		<h3>Task Details</h3>
+		<div>{JSON.stringify(task.task.task_details)}</div>
+
+		<button on:click={() => nearWallet.accept_invite({ task_id: task.task_id })}
+			>Accept Invite</button
+		>
+	{/each} -->
+{/await}
